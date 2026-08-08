34548e38686505daf570a1383e542c1d5a7a774d
Chris Weed <chris@cjweed.com>
2026-08-08 13:48:21 -0500

    docs: auto-generate documentation for riknoll/arcade-overworld

--- content/extensions/index.ts
+++ content/extensions/index.ts
@@ -1,7 +1,11 @@
 import type { ExtensionDoc, ExtensionTool } from "@/content/extensions/types";
+import { arcadeOverworld } from "@/content/extensions/riknoll/arcade-overworld";
 import { arcadeSpriteUtil } from "@/content/extensions/jwunderl/arcade-sprite-util";
 
-export const extensions: ExtensionDoc[] = [arcadeSpriteUtil];
+export const extensions: ExtensionDoc[] = [
+  arcadeSpriteUtil,
+  arcadeOverworld,
+];
 
 export function getExtension(owner: string, repo: string): ExtensionDoc | undefined {
   return extensions.find((extension) => extension.owner === owner && extension.repo === repo);
