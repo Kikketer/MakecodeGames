5cf6f64d967c379e626cbbf9ffe6e33b4d008496
Chris Weed <chris@cjweed.com>
2026-08-16 11:47:08 -0500

    docs: auto-generate documentation for robo-technical-group/pxt-arcade-vector-math

--- content/extensions/index.ts
+++ content/extensions/index.ts
@@ -1,4 +1,5 @@
 import type { ExtensionDoc, ExtensionTool } from "@/content/extensions/types";
+import { pxtArcadeVectorMath } from "@/content/extensions/robo-technical-group/pxt-arcade-vector-math";
 import { spriteWalls } from "@/content/extensions/Sonicblaston62/Sprite-Walls";
 import { arcadeCameraOffset } from "@/content/extensions/riknoll/arcade-camera-offset";
 import { arcadeSplitScreen } from "@/content/extensions/riknoll/arcade-split-screen";
@@ -9,6 +10,7 @@ export const extensions: ExtensionDoc[] = [
   arcadeSplitScreen,
   arcadeCameraOffset,
   spriteWalls,
+  pxtArcadeVectorMath,
 ];
 
 export function getExtension(owner: string, repo: string): ExtensionDoc | undefined {
