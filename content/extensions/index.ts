924538120310356bd068aadc374ae1bbde7a87db
Chris Weed <chris@cjweed.com>
2026-08-16 11:07:22 -0500

    docs: auto-generate documentation for riknoll/arcade-camera-offset (#67)

--- content/extensions/index.ts
+++ content/extensions/index.ts
@@ -1,10 +1,12 @@
 import type { ExtensionDoc, ExtensionTool } from "@/content/extensions/types";
+import { arcadeCameraOffset } from "@/content/extensions/riknoll/arcade-camera-offset";
 import { arcadeSplitScreen } from "@/content/extensions/riknoll/arcade-split-screen";
 import { arcadeSpriteUtil } from "@/content/extensions/jwunderl/arcade-sprite-util";
 
 export const extensions: ExtensionDoc[] = [
   arcadeSpriteUtil,
   arcadeSplitScreen,
+  arcadeCameraOffset,
 ];
 
 export function getExtension(owner: string, repo: string): ExtensionDoc | undefined {
