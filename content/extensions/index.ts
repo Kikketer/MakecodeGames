a35051f2857b09aa5c630924ca85dfff637f52fe
Chris Weed <chris@cjweed.com>
2026-08-16 11:22:53 -0500

    docs: auto-generate documentation for UnicycleDumpTruck/pxt-image-morph

--- content/extensions/index.ts
+++ content/extensions/index.ts
@@ -1,4 +1,5 @@
 import type { ExtensionDoc, ExtensionTool } from "@/content/extensions/types";
+import { pxtImageMorph } from "@/content/extensions/UnicycleDumpTruck/pxt-image-morph";
 import { spriteWalls } from "@/content/extensions/Sonicblaston62/Sprite-Walls";
 import { arcadeCameraOffset } from "@/content/extensions/riknoll/arcade-camera-offset";
 import { arcadeSplitScreen } from "@/content/extensions/riknoll/arcade-split-screen";
@@ -9,6 +10,7 @@ export const extensions: ExtensionDoc[] = [
   arcadeSplitScreen,
   arcadeCameraOffset,
   spriteWalls,
+  pxtImageMorph,
 ];
 
 export function getExtension(owner: string, repo: string): ExtensionDoc | undefined {
