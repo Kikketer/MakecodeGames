a010c433b560fdffc4a421c47f67d8447ccb3e11
Chris Weed <chris@cjweed.com>
2026-08-16 09:47:08 -0500

    docs: auto-generate documentation for CrzLe0723/RetroFx

--- content/extensions/index.ts
+++ content/extensions/index.ts
@@ -1,10 +1,12 @@
 import type { ExtensionDoc, ExtensionTool } from "@/content/extensions/types";
+import { retrofx } from "@/content/extensions/CrzLe0723/RetroFx";
 import { arcadeSplitScreen } from "@/content/extensions/riknoll/arcade-split-screen";
 import { arcadeSpriteUtil } from "@/content/extensions/jwunderl/arcade-sprite-util";
 
 export const extensions: ExtensionDoc[] = [
   arcadeSpriteUtil,
-  arcadeSplitScreen,
+  arcadeSplitScreen,,
+  retrofx,
 ];
 
 export function getExtension(owner: string, repo: string): ExtensionDoc | undefined {
